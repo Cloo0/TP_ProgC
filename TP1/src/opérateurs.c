2214b3774e29c0b56f31133dc9d70d5c6eb3ffd3
Clothilde.Delanoy <clothildedelanoy@gmail.com>
2026-01-29 10:32:56 +0100

    ex05 : opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,6 +1,27 @@
+#include <stdio.h>
+void calcul(int a,int b) {
+    printf("a+b = %d\n",a+b);
+    printf("a-b =%d\n ",a-b);
+    printf("a*b =%d\n",a*b);
+    printf("a/b =%d\n",a/b);
+    printf("Le reste de la division de a par b est %d\n",a%b);
+    if (a==b) {
+        printf("True\n");
+    }
+    else {
+        printf("False\n");
+    }
+    if (a>b) {
+        printf("True\n");
+    }
+    else {
+        printf("False\n");
+    }
+}
 int main() {
-int a = 5;
-
+    int a = 16;
+    int b = 3;
+    calcul(a,b);
     return 0;
 }
 
