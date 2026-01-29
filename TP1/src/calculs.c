458b297bccda2fe3dde396858e3ad3b1db095c80
Clothilde.Delanoy <clothildedelanoy@gmail.com>
2026-01-29 12:11:27 +0100

    ex08 : calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main () {
+    int num1 = 12;
+    int num2 = 4;
+    char op = '/';
+    switch (op) {
+        case '+':
+            printf("%d",num1 + num2);
+            break;
+        case '-':
+            printf("%d",num1 - num2);
+        break;
+        case '*':
+            printf("%d",num1 * num2);
+            break;
+        case '/':
+            printf("%d",num1 / num2);
+            break;
+    }
+    return 0;
+}
