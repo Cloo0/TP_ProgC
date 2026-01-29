90f32579755184f53885d3b2bd676f8a803c236f
Clothilde.Delanoy <clothildedelanoy@gmail.com>
2026-01-29 11:31:33 +0100

    ex06 : boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 int main() {
+   int k = 0;
    for(int i = 0; i < 10; i++) {
       for(int j = 0; j < i; j++) {
+          printf("*");
+       }
+       printf("\n");
+    }
+   while ( k < 10) {
+      int l = 0;
+      while ( l <= k) {
          printf("*");
+         l++;
       }
       printf("\n");
+      k++;
    }
    return 0;
 }
